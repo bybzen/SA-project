5c11edfdde80e62da18fea4c5af0e9f099e119b0
Thankorn Rattrakulthip <thanakorn.ratt@ku.th>
2021-10-27 22:24:43 +0700

    Merge Conflict

--- src/main/java/shop/controllers/admin/CreateWorkorderController.java
+++ src/main/java/shop/controllers/admin/CreateWorkorderController.java
@@ -46,12 +46,6 @@ public class CreateWorkorderController {
     CheckAndAlert ch = new CheckAndAlert();
     Connection con;
 
-//<<<<<<< HEAD
-//
-//    @FXML public void initialize() {
-//=======
-//>>>>>>> 13b1d1d2b2da084e524c8eadc6e3835359229736
-
     @FXML
     public void initialize() throws SQLException {
 
